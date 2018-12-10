102d019b7c9b5769145b880cb264cd6688bd26fc
avitenzer <tenzer@clara.co.uk>
2018-12-10 18:44:15 +0200

    Updated create miner script

--- create_miner_node.sh
+++ create_miner_node.sh
@@ -6,8 +6,8 @@ sudo apt install git &&
 
 curl -L https://www.opscode.com/chef/install.sh | sudo bash &&
 
-git clone https://github.com/avitenzer/db-chef.git &&
+git clone https://github.com/dreamAlpha/blockchain.git &&
 
-cd db-chef/node-linux &&
+cd blockchain/node-linux &&
 
 sudo sh install_node &&
